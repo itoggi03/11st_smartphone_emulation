a4c61c5bffc65bb87a1d07c9eb20a4b04f8618f5
itoggi03 <itoggi03@gmail.com>
2021-08-23 02:08:05 +0900

    feat: Add memo & save localstorage

--- src/index.js
+++ src/index.js
@@ -42,27 +42,58 @@ window.addEventListener("hashchange", () => {
   location.hash == "#memo" && onMemo();
 });
 
-
 /* 
   memo 페이지에서..
   1. new 버튼 클릭시 input 나타남
-  2. input창에서 값 입력 후 엔터 누르면 입력값 LocalStorage에 저장 후 input 사라짐
+  2. input창에서 값 입력 후 엔터 누르면 입력값 LocalStorage에 저장
   3. 입력한 값들의 목록을 보여줌
+  4. input 사라짐 & input 값 초기화
 */
+let id = 0;
+
 function onMemo() {
   const memoInput = document.querySelector(".memo-input");
   const newBtn = document.querySelector(".new");
-
+  getMemo();
   newBtn.addEventListener("click", () => {
     memoInput.classList.remove("memo-input-hidden");
+    memoInput.focus();
   });
 
   memoInput.addEventListener("keydown", (event) => {
-    if (event.key !== "Enter") {
+    if (event.key !== "Enter" || memoInput.value === "") {
       return;
     }
     // 1. LocalStorage에 입력 내용 저장
-    // 2. 입력창 사라짐
+    localStorage.setItem(id, memoInput.value);
+    id++;
+
+    // 2. 입력한 값들의 목록을 보여줌
+    const ulTag = document.querySelector(".items");
+    const item = createItem(memoInput.value);
+    ulTag.appendChild(item);
+
+    // 3. 입력창 사라짐 & 인풋 값 초기화
     memoInput.classList.add("memo-input-hidden");
+    memoInput.value = "";
   });
+}
+
+function getMemo() {
+  const ulTag = document.querySelector(".items");
+
+  Object.keys(localStorage).forEach((memoId) => {
+    const item = createItem(localStorage.getItem(memoId));
+    ulTag.appendChild(item);
+  });
+}
+
+function createItem(text) {
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "item__row");
+  itemRow.innerHTML = `<li class="item__row">
+  <div class="item">${text}</div>
+  <div class="item__divider"></div>
+</li>`;
+  return itemRow;
 }
