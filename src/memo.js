dace3118b50fd8d6dcd3c14f69cb146f43375f88
itoggi03 <itoggi03@gmail.com>
2021-08-23 22:33:41 +0900

    fix: 메모 펼치는 기능의 오류, 메모 아이디 오류 수정

--- src/memo.js
+++ src/memo.js
@@ -1,10 +1,13 @@
 /* 
   memo 페이지에서..
-  1. new 버튼 클릭시 input 나타남
-  2. input창에서 값 입력 후 엔터 누르면 입력값 LocalStorage에 저장
-  3. 입력한 값들의 목록을 보여줌
-  4. input 사라짐 & input 값 초기화
+  - 저장된 메모 목록 보여주기
+  - new 버튼 클릭시 input 나타남
+  - input창에서 값 입력 후 엔터 누르면 입력값 LocalStorage에 저장
+  - 입력한 값들의 목록을 보여줌
+  - input 사라짐 & input 값 초기화
+  - 메모 클릭시 1개만 펼쳐짐
 */
+
 let id = 0;
 
 module.exports.onMemo = function () {
@@ -23,37 +26,38 @@ module.exports.onMemo = function () {
     if (event.key !== "Enter" || memoInput.value === "") {
       return;
     }
-    // 1. LocalStorage에 입력 내용 저장
+    // LocalStorage에 입력 내용 저장
     localStorage.setItem(`memo${id}`, memoInput.value);
 
-    // 2. 입력한 값들의 목록 보여주기
+    // 입력한 값들의 목록 보여주기
     const ulTag = document.querySelector(".items");
     const inputItem = createItem(memoInput.value, id);
     ulTag.appendChild(inputItem);
     id++;
 
-    // 3. 입력창 사라짐 & 인풋 값 초기화
+    // 입력창 사라짐 & 인풋 값 초기화
     memoInput.classList.add("memo-input-hidden");
     memoInput.value = "";
+
+    onClickMemo();
   });
 
-  // 4. 메모 클릭시 펼쳐지는 함수 실행
+  // 메모 클릭시 펼쳐지는 함수 실행
   onClickMemo();
 };
 
 // localStorage에 저장되어 있는 메모들 보여주는 함수
 function getMemo() {
   const ulTag = document.querySelector(".items");
-  // localStorage.clear();
+
   Object.keys(localStorage).forEach((memoId) => {
     if (memoId.slice(0, 4) == "memo") {
       const currId = Number(memoId.slice(-1));
       const item = createItem(localStorage.getItem(`memo${currId}`), memoId);
-      id = currId > id ? currId + 1 : id;
+      id = currId >= id ? currId + 1 : id;
       ulTag.appendChild(item);
     }
   });
-  console.log(id);
 }
 
 // 메모 아이템 생성하는 함수
@@ -67,13 +71,23 @@ function createItem(text, memoId) {
   return itemRow;
 }
 
+/*  
+메모 클릭시 펼쳐지게 하는 함수
+- 한번에 하나의 메모만 펼칠 수 있도록 이전에 펼쳐진 메모는 다시 2줄만 표시되도록 한다. 
+*/
 function onClickMemo() {
   const items = document.querySelector(".items");
+  const itemDiv = document.querySelectorAll(".item");
   items.addEventListener("click", (event) => {
     const clickMemoId = event.target.dataset.id;
     if (clickMemoId) {
-      const item = document.querySelector(`.item[data-id="${clickMemoId}`);
-      item.classList.toggle("item-overflow");
+      itemDiv.forEach((item) => {
+        if (item.dataset.id == clickMemoId) {
+          item.classList.remove("item-overflow");
+        } else {
+          item.classList.add("item-overflow");
+        }
+      });
     }
   });
 }
