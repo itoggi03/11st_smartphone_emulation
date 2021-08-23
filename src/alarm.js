1e1093047c07a2252e911bb63e7acc66a02dece9
itoggi03 <itoggi03@gmail.com>
2021-08-23 21:38:06 +0900

    feat: 알람 입력, 저장, 삭제 기능 완성

--- src/alarm.js
+++ src/alarm.js
@@ -1,7 +1,12 @@
+let id = 0;
+
 module.exports.onAlarm = function () {
   const newBtn = document.querySelector(".new");
   const alarmInput = document.querySelector(".alarm-input-section");
   const saveBtn = document.querySelector(".save");
+
+  // 저장된 알람 목록 보여주기
+  getAlarm();
 
   newBtn.addEventListener("click", () => {
     alarmInput.classList.remove("alarm-input-hidden");
@@ -9,17 +14,20 @@ module.exports.onAlarm = function () {
 
   saveBtn.addEventListener("click", () => {
     const inputTime = setTime();
-
-    // const alarm = document.querySelector(".alarm");
-    // alarm.innerHTML = inputTime;
-
     const ul = document.querySelector(".alarms");
-    const li = `<li class="alarms__row">
-    <div class="alarm">${inputTime}</div>
-    <button class="delete">삭제</button>
-  </li>`;
-    ul.appendChild(li);   // 여기서 에러남
+
+    // localStorage에 저장
+    localStorage.setItem(`alarm${id}`, inputTime);
+
+    // 알람 목록 보여주기
+    const li = createItem(inputTime, id);
+    ul.append(li);
+    id++;
+
+    // input 사라짐
+    alarmInput.classList.add("alarm-input-hidden");
   });
+  onClickDelete();
 };
 
 // 입력한 시간을 가공하여 보여줄 시간 구하는 함수
@@ -37,5 +45,43 @@ function setTime() {
     am = false;
     hour = hour - 12;
   }
-  return am == true ? `오전 ${hour}시 ${min}분` : `오후 ${hour}시 ${min}분`;
+
+  const result =
+    am == true ? `오전 ${hour}시 ${min}분` : `오후 ${hour}시 ${min}분`;
+  return result;
+}
+
+function getAlarm() {
+  Object.keys(localStorage).forEach((alarmId) => {
+    const ul = document.querySelector(".alarms");
+    if (alarmId.slice(0, 5) == "alarm") {
+      const currId = Number(alarmId.slice(-1));
+      const item = createItem(localStorage.getItem(`alarm${currId}`), alarmId);
+      
+      id = currId > id ? currId + 1 : id;
+      ul.appendChild(item);
+    }
+  });
+}
+
+function createItem(time, alarmId) {
+  const li = document.createElement("li");
+  li.setAttribute("class", "alarms__row");
+  li.innerHTML = `<div class="alarm">${time}</div>
+  <button class="delete" data-id=${alarmId}>삭제</button>`;
+  return li;
+}
+
+function onClickDelete() {
+  const items = document.querySelector(".alarms");
+
+  items.addEventListener("click", (event) => {
+    const clickAlarmId = event.target.dataset.id;
+    if (clickAlarmId) {
+      const item = document.querySelector(`.delete[data-id="${clickAlarmId}"]`);
+
+      localStorage.removeItem(clickAlarmId);
+      items.removeChild(item.parentElement);
+    }
+  });
 }
